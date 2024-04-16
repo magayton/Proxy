6b7786aee2dfa22afc09cdcfa73c6304253443e6
Mathis Gayton <mathis.gayton@gmail.com>
2024-04-16 10:34:38 +0200

    hardhat proxy project from alchemy

---
+++ scripts/upgradeProxy.js
@@ -0,0 +1,17 @@
+const { ethers, upgrades } = require('hardhat');
+
+const proxyAddress = '0xDCC4A3f000CBc124F6fBEc384aDd5624c1C7c25D';
+
+async function main() {
+  const VendingMachineV2 = await ethers.getContractFactory('VendingMachineV2');
+  const upgraded = await upgrades.upgradeProxy(proxyAddress, VendingMachineV2);
+
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+    proxyAddress
+  );
+
+  console.log("The current contract owner is: " + await upgraded.owner());
+  console.log('Implementation contract address: ' + implementationAddress);
+}
+
+main();
